18ee2455185d4171189811b5b45617767e24c34f
Sham E Ansari <46960972+Sham-E-Ansari@users.noreply.github.com>
2021-11-17 16:04:29 +0600

    todoapp update

---
+++ src/components/TodoForm.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, useRef} from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const todoFocus = useRef(null);
+    useEffect(()=>{
+        todoFocus.current.focus();
+    })
+
+
+    const handleChange=(e)=>{
+        setInput(e.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random()*10000),
+            text: input
+        });
+    };
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder ="Add a todo" 
+                value={input} 
+                name='text' 
+                className="todo-input"
+                onChange={handleChange}
+                ref={todoFocus}
+            />
+            <button className="todo-btn">
+                Add ToDo
+            </button>
+        </form>
+    )
+}
+
+export default TodoForm
